e1e3b12b69f8ed7812fe9adf6d5dac6ed9e7f957
irlisson-23ferreira <irlissondias@gmail.com>
2026-07-28 21:15:20 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,6 +2,7 @@
 var srtm = ee.Image('USGS/SRTMGL1_003');
 
 // Gerando a declividade (Slope) a partir do relevo em graus
+// A declividade não é utilizada nesse script, está apenas como exemplo
 var declividade = ee.Terrain.slope(srtm);
 
 Map.addLayer(srtm, {min: 0, max: 1000, palette: ['blue', 'yellow', 'red']}, 'Elevação (m)');
